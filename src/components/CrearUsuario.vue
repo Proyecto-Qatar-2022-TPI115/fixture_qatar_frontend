759495c4e9970e7416d475be558f44c02569c14c
Alexander <57245999+alexanderapp07@users.noreply.github.com>
2022-12-01 09:59:40 -0600

    Aplicación de estilos en componente CrearUsuario

--- src/components/CrearUsuario.vue
+++ src/components/CrearUsuario.vue
@@ -1,39 +1,41 @@
 <template>
-    <h2>Bienvenido</h2>
-    <form @submit.prevent="signup">
-        <div class="cont">
-            <h2>Crear perfil</h2>
-            <p>
-                <label for="iname">Nombre</label>
-                <input id="iname " type="text" name=" nombre " v-model="nombre"/>
-            </p>
+    <body class="crear">
+        <h2>Bienvenido</h2>
+        <form @submit.prevent="signup">
+            <div class="cont">
+                <h2>Crear perfil</h2>
+                <p>
+                    <label for="iname">Nombre</label>
+                    <input id="iname " type="text" name=" nombre " v-model="nombre"/>
+                </p>
 
-            <p>
-                <label for="iapellido">Apellido</label>
-                <input id="iapellido " type="text" name=" nombre " v-model="apellido"/>
-            </p>
-            <p>
-                <label for="inick">Correo</label>
-                <input id="inick" type="text" name=" nombre " v-model="email"/>
-            </p>
-            <p>
-                <label for="ipass">Contraseña</label>
-                <input id="ipass " type="text" name=" nombre " v-model="password"/>
-            </p>
-            <div class="dbotones">
-                <input type="submit" value=" Aceptar " />
-                <input type="submit" value=" Rechazar " />
+                <p>
+                    <label for="iapellido">Apellido</label>
+                    <input id="iapellido " type="text" name=" nombre " v-model="apellido"/>
+                </p>
+                <p>
+                    <label for="inick">Correo</label>
+                    <input id="inick" type="text" name=" nombre " v-model="email"/>
+                </p>
+                <p>
+                    <label for="ipass">Contraseña</label>
+                    <input id="ipass " type="text" name=" nombre " v-model="password"/>
+                </p>
+                <div class="dbotones">
+                    <input type="submit" value=" Aceptar " />
+                    <input type="submit" value=" Rechazar " />
+                </div>
             </div>
-        </div>
-        <br />
-        <div class="subirImagen">
-            <h2>Arrastra y suelta una imagen</h2>
-            <span>0</span>
-            <button>selecciona tus archivos</button>
-            <input type="file" id="input-file" hidden multiple />
-        </div>
-        <div id="preview"></div>
-    </form>
+            <br />
+            <div class="subirImagen">
+                <h2>Arrastra y suelta una imagen</h2>
+                <span>0</span>
+                <button>selecciona tus archivos</button>
+                <input type="file" id="input-file" hidden multiple />
+            </div>
+            <div id="preview"></div>
+        </form>
+    </body>
 </template>
 
 <script>
@@ -70,3 +72,7 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    @import '../assets/css/estilos.css';
+</style>
